64a724875a271301aa25ab35738430777debf923
sunjc <sunjc@iata.org>
2020-03-25 13:45:37 +0800

    add log configuration

--- src/main/java/org/itrunner/openfeign/config/GitHubConfiguration.java
+++ src/main/java/org/itrunner/openfeign/config/GitHubConfiguration.java
@@ -1,9 +1,15 @@
 package org.itrunner.openfeign.config;
 
+import feign.Logger;
 import feign.auth.BasicAuthRequestInterceptor;
 import org.springframework.context.annotation.Bean;
 
 public class GitHubConfiguration {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
         return new BasicAuthRequestInterceptor("sunjc", "a8e117f1dcc6076cb0441940af01875c2f6ca7b3");
